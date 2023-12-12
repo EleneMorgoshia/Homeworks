1bedb9a0cc65ece8b5d553d5fc867f924928fa86
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2023-12-12 16:42:32 +0400

    forth commit work1

---
+++ lesson3-new/HOMEWORK-N1/WORK1.py
@@ -0,0 +1,37 @@
+#classwork
+
+mum_age=50
+print(mum_age)
+dad_age=55
+print(dad_age)
+Nika_age=16
+print(Nika_age)
+Demetre_age=12
+print(Demetre_age)
+grandma_age=78
+print(grandma_age)
+grandpa_age=82
+print(grandpa_age)
+
+#after 5 years
+string1="----------------------"
+print(string1)
+mum_age=55
+print(mum_age)
+dad_age=60
+print(dad_age)
+Nika_age=21
+print(Nika_age)
+Demetre_age=17
+print(Demetre_age)
+grandma_age=83
+print(grandma_age)
+grandpa_age=87
+print(grandpa_age)
+
+#Also you can write  like that: mum_age+=5 
+
+#Home work: 1)new words +2) quise+
+#new words:  1)variable-ცვლადი
+#            2)value-მნიშვნელობა
+#             3)quotation marks-""

7ecc469ecca3aff3e1c55b7349a7c3fb65b54af3
EleneMorgoshia <elenemorgoshia.20049@gmail.com>
2023-12-13 19:20:01 +0400

    third commit

---
+++ lesson3/Day3/classwork/classwork.py
@@ -0,0 +1,94 @@
+#Group 4
+#----------------------
+# name="Tom"
+# salary=1000
+# print(name)
+# #print word "name"
+# print("name")
+# name="name"
+# print(name)
+#----------------------
+#here we learned about debugging
+# message="Debbuging"
+# print(message)
+#-----------------------
+#home work - quise 2
+#-------------------------
+#Group 102
+
+#format
+# my_name="Elene"
+# my_surname="Morgoshia"
+# my_age=19
+# my_pet_name="miki"
+# my_sentence="My name is {0}, My username is {1}, My petname is {2}, My age is {3}.".format(my_name, my_surname , my_pet_name,my_age)
+# print(my_sentence)
+#----------------------------
+#if else
+# my_name="Elene"
+# if "e"in my_name:
+#     print("letter e is in my_name")
+# else:
+#     print("letter e is not in my_name")
+#----------------------------------
+#input and output 
+#input-Data given to a program.(mouse)
+#output- Result produced by the program. (monitor)
+# my_name=input("enter your name: ") #your name value  will be saved in my_name variable
+# print("My name is"+ " " + my_name)
+#--------------------------------------
+#guideline of incresing values
+# my_age=19
+# #after 3 year
+# my_age+=3
+# print(my_age)
+#------------------------------------------
+#home work: ask user to enter his/her name,surname,age
+#            print this variables as a beauiful sentence ( use format method)
+#             print what will heppen after 3 years
+
+# your_name=input("Enter your name pls: ") #input function always has string type value as a parameter !
+# your_surname=int(input("Enter your surname pls: "))
+# your_age=(input("Enter your age pls: ")) # add int in order to have integer type for age 
+# print(int(your_age))
+# beautiful_sentence="My name is {0}, My surname is {1}, I am {2} years old ".format(your_name,your_surname,your_age)
+# print(beautiful_sentence)
+# increase_age=("What will happen after 3 years?")
+# print(increase_age) #what is happening-explanation in line 62
+# print(type(your_age))
+# your_age=int(your_age)+3 # rewrite  variable. changeing  types.  
+# another_beautiful_sentence="After 3 years,my age will be: {}".format(your_age)
+# print(another_beautiful_sentence)
+
+#how to make values as a same types
+# print(2+2)
+# print("2"+2)#error.different type
+# print("2"+"2"+"2")#222
+# print(int("2")+2)
+#---------------------------------------------
+#home work N2: aks user to enter 2 number.
+# if multiplication of these numbers is grater thant 100, print multiplication,
+# or print :" you lose the game "
+
+# first_number=int(input("enter the number pls: "))
+# second_number=int(input("enter another number pls "))
+# if first_number*second_number>100:
+#        print(first_number*second_number)
+# else :
+#        print("you lose:(")
+
+#another way:
+# num1=int(input("enter num1: "))
+# num2=int(input("enter num2: "))
+# product=num1*num2
+# if product>100:
+#    print(product)
+# else :
+#       print("you lose")
+#--------------------------------
+#% operator
+# print(20%6) # 6*3=18, 20-18=2 , so,result will be 2
+
+#homework:
+#use  input, %,if ,+=  and 
+#ask user to enter 3 numbers.then you should sum the odd numbers and print . 
---
+++ lesson3/Day3/homework/homework.py
@@ -0,0 +1,32 @@
+#homework:
+#use  input, %,if ,+= ; and 
+#ask user to enter 3 numbers.then you should sum the odd numbers and print . 
+num1=int(input("enter num1: "))
+num2=int(input("enter num2: "))
+num3=int(input("enter num3: "))
+
+variable1=num1%2
+if variable1==1:
+    print("num1 is an odd number")
+    variable1=num1
+else:
+    print("num1 is not an  odd number")
+    variable1=0
+
+variable2=num2%2
+if variable2==1:
+       print("num1 is an  odd number")
+       variable2=num2
+else:
+    print("num1 is not an odd number")
+    variable2=0
+
+variable3=num3%2
+if variable3==1:
+     print("num1 is an odd number")
+     variable3=num3
+else:
+    print("num1 is not an  odd number")
+    variable3=0
+
+print(variable1+variable2+variable3)
